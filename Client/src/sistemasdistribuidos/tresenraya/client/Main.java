49750536befba2fcc17f477edb3e00174ec09b79
Jonathan Yajamín <jonatan_2994@hotmail.com>
2017-06-28 20:57:39 +0200

    Create main window.

---
+++ Client/src/sistemasdistribuidos/tresenraya/client/Main.java
@@ -0,0 +1,16 @@
+package sistemasdistribuidos.tresenraya.client;
+
+import javax.swing.JFrame;
+
+import sistemasdistribuidos.tresenraya.client.views.Board;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Board board = new Board("Tablero");
+		board.setSize(400, 300);
+		board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		board.setVisible(true);
+	}
+
+}
--- Client/src/sistemasdistribuidos/tresenraya/client/views/Board.java
+++ Client/src/sistemasdistribuidos/tresenraya/client/views/Board.java
@@ -1,5 +1,26 @@
 package sistemasdistribuidos.tresenraya.client.views;
 
-public class Board {
+import javax.swing.JFrame;
+
+public class Board extends javax.swing.JFrame {
+	
+	private javax.swing.JButton cell1;
+    private javax.swing.JButton cell2;
+    private javax.swing.JButton cell3;
+    private javax.swing.JButton cell4;
+    private javax.swing.JButton cell5;
+    private javax.swing.JButton cell6;
+    private javax.swing.JButton cell7;
+    private javax.swing.JButton cell8;
+    private javax.swing.JButton cell9;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Board(String name){
+		super(name);
+	}
 
 }
