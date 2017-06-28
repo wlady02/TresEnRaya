bf48c1a96ec43a0e6661539d083c81974c121c9f
Jonathan Yajamín <jonatan_2994@hotmail.com>
2017-06-29 00:00:19 +0200

    Remove unused imports.

--- Client/src/sistemasdistribuidos/tresenraya/client/views/Board.java
+++ Client/src/sistemasdistribuidos/tresenraya/client/views/Board.java
@@ -1,9 +1,7 @@
 package sistemasdistribuidos.tresenraya.client.views;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
