91ad9fa2506b82a01d2c203d255bec63c1bd524d
Jonathan Yajamín <jonatan_2994@hotmail.com>
2017-07-14 13:38:47 +0200

    Comentarios

--- TicTacToe/src/ClientTicTacToe/IGame.java
+++ TicTacToe/src/ClientTicTacToe/IGame.java
@@ -5,4 +5,12 @@ public interface IGame {
 		public void setMove(int pos, int value);
 		public int victoria();
 		public void reiniciar();
+	
+	// Hay una cosa que no entiendo. El juego es multijugador o es una persona contra el servidor (máquina)?
+	// Si es lo segundo, solo necesitas tener una función de realizar movimiento en la partida (Game) en el servidor. 
+	// Este método remoto te tiene que devolver si es correcto el movimiento. Si lo es lo pintas en el tablero, sino 
+	// lo notificas.
+	
+	// No entiendo para que tienes getMove(), victoria() y reiniciar(). Cuando acabe la partida, este objeto se elimina 
+	// y se crea una nueva instancia.
 }
